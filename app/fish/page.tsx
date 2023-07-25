44787028d7eff14d8b2589dac338f352d2fae5df
Carlos Rodriguez <106417568+Crod7@users.noreply.github.com>
2023-07-25 05:15:06 -0700

    Changed the structure of my project. I made the fetch req that gets 3 random fish into a component instead of a page. This seems to have a better user experience, I will see if this resolves the issue.

--- app/fish/page.tsx
+++ app/fish/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import getAllFish from '@/lib/getAllFishes'
 import Link from "next/link"
+import { Suspense } from "react"
+
+import FishSelect from './components/FishSelect'
 
 export const metadata: Metadata = {
     title: 'Fishes',
@@ -9,45 +12,11 @@ export const metadata: Metadata = {
 export default async function FishPage() {
     const fishData: Promise<Fish[]> = getAllFish()
 
+    return (
+        <Suspense fallback={<h2>Loading...</h2>}>
+            <FishSelect promise={fishData} />
+        </Suspense>
 
-    const fishes = await fishData
-
-
-    const content = (
-        <section>
-            <br />
-            <div className='button-center'>
-
-                <button className='button-box' >
-                    <a href='/fish'>
-                        Generate More Fish
-                    </a>
-                </button>
-            </div>
-            <br />
-
-            <div  className='fish-select-page-layout'>
-                {Array.from({ length: 3 }).map((_, index) => {
-                    const randomFish = fishes[Math.floor(Math.random() * 1100)];
-                    console.log(randomFish.id)
-
-                    return (
-                        <div key={randomFish.id}>
-                            <Link href={`/fish/${randomFish.name}`}>
-                                <p>
-                                    {randomFish.name}
-                                </p>
-                                <img className='image-small' src={randomFish.img_src_set['2x']} alt='[[[ IMAGE NOT AVAIABLE ]]]' />
-                            </Link>
-                            <br />
-                        </div>
-                    )
-                })}
-            </div>
-            
-
-        </section>
-    )
-
-    return content
+        
+    );
 }
