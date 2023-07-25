367706cc42b7ac0b5e48d1890b887fab1cab038c
Carlos Rodriguez <106417568+Crod7@users.noreply.github.com>
2023-07-25 06:58:38 -0700

    Figured out the issue. All fetch reqs are static. They need to be dynamic for my particular use case so I changed the api req by adding custom options.

--- lib/getAllFishes.ts
+++ lib/getAllFishes.ts
@@ -1,20 +1,17 @@
 export default async function getAllFish() {
-    const randomQueryParam = `cacheBuster=${Date.now()}`;
-    const url = `https://fish-species.p.rapidapi.com/fish_api/fishes?${randomQueryParam}`;
-    const options = {
+  const url = `https://fish-species.p.rapidapi.com/fish_api/fishes`;
+
+  try {
+    const response = await fetch(url, {
+      cache: 'no-store',
       method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'fc8f10559amsh7d96aa4d765690fp1b200fjsnf920b668d518',
-        'X-RapidAPI-Host': 'fish-species.p.rapidapi.com',
-        'Cache-Control': 'no-cache',
-      },
-    };
-  
-    try {
-      const response = await fetch(url, options);
-      return response.json();
-    } catch (error) {
-      console.error(error);
-    }
+      headers:{
+      'X-RapidAPI-Key': 'fc8f10559amsh7d96aa4d765690fp1b200fjsnf920b668d518',
+      'X-RapidAPI-Host': 'fish-species.p.rapidapi.com',
+      }
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
   }
-  
+}
