3b55536529ec43f3fcb14f9c0188d40534d66718
Carlos Rodriguez <106417568+Crod7@users.noreply.github.com>
2023-07-25 04:38:06 -0700

    Attempting to diable caching on fetch requests.

--- lib/getAllFishes.ts
+++ lib/getAllFishes.ts
@@ -2,17 +2,19 @@ export default async function getAllFish() {
     const randomQueryParam = `cacheBuster=${Date.now()}`;
     const url = `https://fish-species.p.rapidapi.com/fish_api/fishes?${randomQueryParam}`;
     const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'fc8f10559amsh7d96aa4d765690fp1b200fjsnf920b668d518',
-            'X-RapidAPI-Host': 'fish-species.p.rapidapi.com'
-        }
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'fc8f10559amsh7d96aa4d765690fp1b200fjsnf920b668d518',
+        'X-RapidAPI-Host': 'fish-species.p.rapidapi.com',
+        'Cache-Control': 'no-cache',
+      },
     };
-    
+  
     try {
-        const response = await fetch(url, options);
-        return response.json();
+      const response = await fetch(url, options);
+      return response.json();
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-}
+  }
+  
