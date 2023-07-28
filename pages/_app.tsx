b5039af9372af9c49c8e56c1fd3af8c0b98aad93
Carlos Rodriguez <106417568+Crod7@users.noreply.github.com>
2023-07-28 04:04:02 -0700

    Works in build, checking to see if works in production.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
+    <UserProvider loginUrl="/api/auth/login" profileUrl="/api/auth/me">
       <Component {...pageProps} />
     </UserProvider>
   );
