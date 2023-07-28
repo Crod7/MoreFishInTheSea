abe2d26fd3b66e9f61bdda96284a3721b79eb4d1
Carlos Rodriguez <106417568+Crod7@users.noreply.github.com>
2023-07-28 09:20:55 -0700

    Changed directory of project to include src folder, added mongoDB so that users are saved to a database. Added axios to make api calls to add users. Added user schema etc.

---
+++ src/models/UserModel.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    family_name: {
+        type: String,
+        required: false,
+    },
+    given_name: {
+        type: String,
+        required: false,
+    },
+    nickname: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    picture: {
+        type: String,
+        required: false,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from "next/link";
+import axios from 'axios';
 
 export default function Profile() {
   const { user, error, isLoading } = useUser();
+
+  // Used for user registration if not in database
+  const registerUser = async () => {
+    try {
+      const userData = {
+        nickname: user?.nickname,
+        family_name: user?.family_name,
+        given_name: user?.given_name,
+        picture: user?.picture,
+      }
+
+      const response = await axios.post('/api/set_user', { user: userData })
+    }catch(err){
+      console.log(err);
+    }
+  }
+
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
   if (user) {
+
+    // If the user successfully logs in, we either create or verify that the user exists in the database with axios
+    registerUser();
+
     console.log(user);
     const pictureSrc = user.picture ?? 'none';
     return (
